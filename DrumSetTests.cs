c0df35eefc4324c18b794b432f7883042b39b4b0
BoyanaAngelova <135323451+BoyanaAngelova@users.noreply.github.com>
2024-02-26 15:56:09 +0200

    Add files via upload

--- DrumSetTests.cs
+++ DrumSetTests.cs
@@ -9,31 +9,81 @@ public class DrumSetTests
 {
     [Test]
     public void Test_Drum_TerminateCommandNotGiven_ThrowsArgumentException()
-    {
-        // TODO: implement the test
+    {    
+        //Arrange
+        decimal money = 100;
+        List<int> initialQuality = new() { 2, 3 };
+        List<string> commands =  new () { "1", "1" };
+        //Act & Assert
+        Assert.That(() => DrumSet.Drum(money, initialQuality, commands), Throws.ArgumentException);
     }
 
     [Test]
     public void Test_Drum_StringGivenAsCommand_ThrowsArgumentException()
     {
-        // TODO: implement the test
+        // Arrange
+        decimal money = 100;
+        List<int> initialQuality = new() { 2, 3 };
+        List<string> commands = new() { "1", "string", "Hit it again, Gabsy!" };
+        //Act & Assert
+        Assert.That(() => DrumSet.Drum(money, initialQuality, commands), Throws.ArgumentException);
+    }
+        [Test]
+    public void Test_Drum_ReturnsCorrectQualityAndAmount()
+    {
+        decimal money = 100;
+        List<int> initialQuality = new() { 3, 4 };
+        List<string> commands = new() { "1", "1", "Hit it again, Gabsy!" };
+        string expected = "1 2\nGabsy has 100.00lv.";
+        //Act  
+         string result = DrumSet.Drum(money, initialQuality, commands);
+        //Arrange
+        Assert.That(result, Is.EqualTo(expected));
+       
     }
 
     [Test]
-    public void Test_Drum_ReturnsCorrectQualityAndAmount()
+    public void Test_Drum_BrokenDrumReturnsCorrectQualityAndAmount()
     {
-        // TODO: implement the test
+        decimal money = 100;
+        List<int> initialQuality = new() { 3, 10 };
+        List<string> commands = new() { "2", "2", "Hit it again, Gabsy!" };
+        string expected = "3 6\nGabsy has 91.00lv.";
+        //Act  
+        string result = DrumSet.Drum(money, initialQuality, commands);
+        //Arrange
+        Assert.That(result, Is.EqualTo(expected));
+
     }
+
 
     [Test]
     public void Test_Drum_BalanceZero_WithOneDrumLeftOver_ReturnsCorrectQualityAndAmount()
     {
-        // TODO: implement the test
+        //Arrange
+        decimal money = 0;
+        List<int> initialQuality = new() { 3, 4, 5 };
+        List<string> commands = new() { "2", "2", "Hit it again, Gabsy!" };
+        string expected = "1\nGabsy has 0.00lv.";
+        //Act  
+        string result = DrumSet.Drum(money, initialQuality, commands);
+        //Assert
+        Assert.That(result, Is.EqualTo(expected));
+
     }
+
 
     [Test]
     public void Test_Drum_NotEnoughBalance_RemovesAllDrums_ReturnsCorrectQualityAndAmount()
     {
-        // TODO: implement the test
+        //Arrange
+        decimal money = 5;
+        List<int> initialQuality = new() { 3, 4, 5 };
+        List<string> commands = new() { "3", "3", "Hit it again, Gabsy!" };
+        string expected = "\nGabsy has 5.00lv.";
+        //Act  
+        string result = DrumSet.Drum(money, initialQuality, commands);
+        //Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 }
