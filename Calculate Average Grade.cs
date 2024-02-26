5b36e8b5ed233a6de091541a987e70b1bfb75aae
BoyanaAngelova <135323451+BoyanaAngelova@users.noreply.github.com>
2024-02-26 16:06:53 +0200

    Add files via upload

---
+++ Calculate Average Grade.cs
@@ -0,0 +1,13 @@
+﻿int countNumber = int.Parse(Console.ReadLine());
+double averageGrade = 0;
+double sumOfGrades = 0;
+for (int i = 1; i <= countNumber; i++)
+{
+    double grade = double.Parse(Console.ReadLine());
+    if (grade > 0)
+    {
+        sumOfGrades = sumOfGrades + grade;
+    }
+    averageGrade = sumOfGrades/countNumber;
+}
+Console.WriteLine($"{averageGrade:F2}");
---
+++ CharacterRangeTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TestApp.Tests;
+
+public class CharacterRangeTests
+{
+    [Test]
+  public void Test_GetRange_WithAAndBInOrder_ReturnsEmptyString()
+    {
+        char char1 = 'A';
+        char char2 = 'B';
+        string result = CharacterRange.GetRange(char1, char2);
+        Assert.That(result, Is.Empty);
+
+
+    }
+
+    [Test]
+    public void Test_GetRange_WithBAndAInOrder_ReturnsEmptyString()
+    {
+        char char1 = 'B';
+        char char2 = 'A';
+        string result = CharacterRange.GetRange(char1, char2);
+        Assert.That(result, Is.Empty);
+
+    }
+
+    [Test]
+    public void Test_GetRange_WithAAndCInOrder_ReturnsB()
+    {
+        char char1 = 'A';
+        char char2 = 'C';
+        string result = CharacterRange.GetRange(char1, char2);
+        Assert.That(result, Is.EqualTo("B"));
+    }
+
+    [Test]
+    public void Test_GetRange_WithDAndGInOrder_Returns_E_F()
+    {
+        char char1 = 'D';
+        char char2 = 'G';
+        string result = CharacterRange.GetRange(char1, char2);
+        Assert.That(result, Is.EqualTo("E F"));
+    }
+
+    [Test]
+    public void Test_GetRange_WithXAndZInOrder_Returns_Y()
+    {
+        char char1 = 'X';
+        char char2 = 'Z';
+        string result = CharacterRange.GetRange(char1, char2);
+        Assert.That(result, Is.EqualTo("Y"));
+    }
+}
