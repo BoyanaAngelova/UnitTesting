bfcc233aab4c4576ce995917ccb99c8d29c6e6b5
BoyanaAngelova <135323451+BoyanaAngelova@users.noreply.github.com>
2024-02-26 15:57:42 +0200

    Add files via upload

---
+++ DuplicatesTests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.UnitTests;
+
+public class DuplicatesTests
+{
+    // TODO: finish test
+    [Test]
+    public void Test_RemoveDuplicates_EmptyArray_ReturnsEmptyArray()
+    {
+        // Arrange
+        int[] numbers = Array.Empty<int>();
+
+        // Act
+        int[] result = Duplicates.RemoveDuplicates(numbers);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    // TODO: finish test
+    [Test]
+    public void Test_RemoveDuplicates_NoDuplicates_ReturnsOriginalArray()
+    {
+        // Arrange
+        int[] numbers = new[] { 1, 2, 3, 4, 5 };
+
+
+        // Act
+        int[] result = Duplicates.RemoveDuplicates(numbers);
+        // Assert
+        Assert.That(result, Is.EqualTo(numbers));
+    }
+
+    [Test]
+    public void Test_RemoveDuplicates_SomeDuplicates_ReturnsUniqueArray()
+    {
+        int[] numbers = new[] { 1, 1, 3, 3, 5 };
+
+
+        // Act
+        int[] result = Duplicates.RemoveDuplicates(numbers);
+        // Assert
+        Assert.That(result, Is.EqualTo(new int[] { 1, 3, 5, }));
+    }
+    [Test]
+    public void Test_RemoveDuplicates_AllDuplicates_ReturnsSingleElementArray()
+    {
+           
+            int[] numbers = new[] { 1, 1, 1, 1 };
+
+
+            // Act
+            int[] result = Duplicates.RemoveDuplicates(numbers);
+            // Assert
+            Assert.That(result, Is.EqualTo(new int[] { 1 }));
+    }
+} 
---
+++ PalindromeTests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+
+using System.Collections.Generic;
+
+namespace TestApp.UnitTests;
+
+public class PalindromeTests
+{
+    // TODO: finish test
+    [Test]
+    public void Test_IsPalindrome_ValidPalindrome_ReturnsTrue()
+    {
+        // Arrange
+        List<string> word = new() { "aha", "boob" };
+        // Act
+        bool result = Palindrome.IsPalindrome(word);
+        // Assert
+        Assert.IsTrue(result);
+    }
+
+    // TODO: finish test
+    [Test]
+    public void Test_IsPalindrome_EmptyList_ReturnsTrue()
+    {
+        // Arrange
+        List<string> word= new();
+
+        // Act
+        bool result = Palindrome.IsPalindrome(word);
+        //
+        Assert.IsTrue(result);
+    }
+
+    [Test]
+    public void Test_IsPalindrome_SingleWord_ReturnsTrue()
+    {
+        List<string> word = new() { "aha" };
+
+        // Act
+        bool result = Palindrome.IsPalindrome(word);
+        //
+        Assert.IsTrue(result);
+    }
+
+    [Test]
+    public void Test_IsPalindrome_NonPalindrome_ReturnsFalse()
+    {
+        List<string> word = new() { "hahaa" };
+
+        // Act
+        bool result = Palindrome.IsPalindrome(word);
+        //
+        Assert.IsFalse(result);
+    }
+
+    [Test]
+    public void Test_IsPalindrome_MixedCasePalindrome_ReturnsTrue()
+    {
+        List<string> word = new() { "hahah", "aha" };
+
+        // Act
+        bool result = Palindrome.IsPalindrome(word);
+        //
+        Assert.IsTrue(result);
+    }
+}
